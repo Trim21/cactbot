4f4cda4d9a55698419856573d9ae276d0411d258
Trim21 <i@trim21.me>
2021-03-05 07:27:26 +0800

    wip: resources/user_config

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,1 +1,13 @@
 export type Lang = 'en' | 'de' | 'fr' | 'ja' | 'cn' | 'ko';
+
+export type OverlayName = 'raidboss' | 'config'
+
+type callOverlayHandler = (param: { call: string; overlay: OverlayName }) => {};
+declare global {
+  interface Window {
+    addOverlayListener: (eventName: string, handler: () => void) => void;
+    callOverlayHandler: callOverlayHandler;
+  }
+}
+
+declare function callOverlayHandler(param: { call: string; overlay: OverlayName }): {};

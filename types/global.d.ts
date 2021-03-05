b97400d51d6f4802dfde7b28945970e864ae49fb
Trim21 <i@trim21.me>
2021-03-05 22:35:42 +0800

    remove unused types

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,13 +1,1 @@
 export type Lang = 'en' | 'de' | 'fr' | 'ja' | 'cn' | 'ko';
-
-export type OverlayName = 'raidboss' | 'config'
-
-type callOverlayHandler = (param: { call: string; overlay: OverlayName }) => {};
-declare global {
-  interface Window {
-    addOverlayListener: (eventName: string, handler: () => void) => void;
-    callOverlayHandler: callOverlayHandler;
-  }
-}
-
-declare function callOverlayHandler(param: { call: string; overlay: OverlayName }): {};
